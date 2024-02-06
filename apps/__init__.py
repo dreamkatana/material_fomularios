e38c0e772ab7d8c29ca9777de753cc4bb083add4
Joao Eduardo <joaoedu@gmail.com>
2024-02-06 08:55:12 -0300

    fix proxy

--- apps/__init__.py
+++ apps/__init__.py
@@ -57,7 +57,7 @@ def configure_database(app):
 
 def create_app(config):
     app = Flask(__name__)
-    app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1)
     Talisman(app)
     app.config.from_object(config)
     register_extensions(app)
